1a8f1564490da2ee30874c9c241f5e92105736cc
Sethupathi Balakrishnan <b.sethupathi@gmail.com>
2020-09-07 15:37:51 +0530

    Add files via upload

---
+++ odd_even/new.cpp
@@ -0,0 +1,37 @@
+
+//bitwise xor practice
+//finds unique number.
+//author : sethupathi
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+
+{
+
+	#ifndef ONLINE_JUDGE
+	freopen("inputs.txt","r",stdin);
+ 	freopen("outputs.txt","w",stdout);
+ 	#endif
+
+
+	// long long  x; cin>>x;
+	// cout<<(x+10)%3<<endl;
+
+ 	int n; cin>>n;
+
+ 	// int ans=0;
+	int no;
+ 	for(int i=0;i<n;i++)
+ 	{
+
+ 		cin>> no;
+ 		if (no&1) cout<<"odd"<<endl;
+ 		else
+ 		cout<<"even"<<endl;
+ 	}
+ 	// cout<<ans;
+	return 0;
+	
+}
