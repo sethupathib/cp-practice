c3dea2db2c8fb473b035431ab7311f96606e3354
Sethupathi <sethu@Sethupathys-MacBook-Pro.local>
2020-09-07 15:13:00 +0530

    first commit

---
+++ new.cpp
@@ -0,0 +1,35 @@
+
+//bitwise xor practice
+//finds unique number.
+//author : sethupathi
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+
+{
+
+	#ifndef ONLINE_JUDGE
+	freopen("inputs.txt","r",stdin);
+ 	freopen("outputs.txt","w",stdout);
+ 	#endif
+
+
+	// long long  x; cin>>x;
+	// cout<<(x+10)%3<<endl;
+
+ 	int n; cin>>n;
+
+ 	int ans=0;
+	int no;
+ 	for(int i=0;i<n;i++)
+ 	{
+
+ 		cin>> no;
+ 		ans = ans^no;
+ 	}
+ 	cout<<ans;
+	return 0;
+	
+}
