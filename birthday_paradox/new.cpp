3036911d07d599a1fbb44f485898c91b40619fb0
Sethupathi Balakrishnan <b.sethupathi@gmail.com>
2020-09-14 14:12:39 +0530

    Birthday Paradox
    Very Interesting Problem. Had fun learning/solving it.

---
+++ birthday_paradox/new.cpp
@@ -0,0 +1,40 @@
+//birthday paradox problem
+//sethupathi
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+
+
+int main()
+{
+	
+	#ifndef ONLINE_JUDGE
+	freopen("inputs.txt","r",stdin);
+ 	freopen("outputs.txt","w",stdout);
+ 	#endif
+    float x=1.0;
+    //x denotes the no. of people having same birthday
+
+    int people =0;
+    int den = 365;
+    int num = 365;
+
+    float p;
+    cin>>p;
+
+    while(x> 1-p)
+    {
+        x= x*(num)/den;
+        people++; num--;
+        cout<<"People--> "<<people<<"\t"<<"Probability--> "<<x<<endl;
+
+    }
+  
+
+
+    
+    
+}
