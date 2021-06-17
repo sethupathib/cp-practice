ba24333be6c55bef7d7776d65d8d0e6f269fc285
Sethupathi Balakrishnan <b.sethupathi@gmail.com>
2021-06-18 01:58:29 +0800

    Create arrayrotation.cpp

---
+++ pep/arrayrotation.cpp
@@ -0,0 +1,53 @@
+//bugs present
+//need to debug
+#include <iostream>
+using namespace std;
+
+void reverse(char *a, int start, int n){
+    int i=start;
+    int j=n-1;
+    while(i<j){
+        int temp = a[i];
+        a[i] =a[j];
+        a[j]= temp;
+        i++;
+        j--;
+    }
+}
+
+void solve(char *a, int n, int k){
+    k = k%n;
+    if(k<0){
+        k+=n;
+    }
+    reverse(a,0,n-k-1);
+    reverse(a,n-k,n-1);
+        for(int i=0;i<n;i++){
+        cout<<a[i]<<" ";
+        
+    }cout<<endl;
+    reverse(a,0,n-1);
+    cout<<endl;
+    
+    cout<<"Reversed k units"<<endl;
+    for(int i=0;i<n;i++){
+        cout<<a[i]<<" ";
+        
+    }
+    
+}
+
+int main() {
+	// your code goes here
+char a1[9] = "abcdefgh";
+// int a1[4] = {1,2,3,4};
+// int n1 = sizeof(a1)/sizeof(int);
+int n1=9;
+int k=3;
+// for(int i=0;i<n1;i++){
+//         cout<<a1[i]<<" ";
+//     }
+
+    solve(a1,n1,k);	
+	return 0;
+}
